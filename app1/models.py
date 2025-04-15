bb94a41254f5be61cbe7b7d4b029fe519c17c2c1
Sudharsan <sudharsans913@gmail.com>
2025-04-15 19:09:07 +0530

    models successfully created for every bottom level role

--- app1/models.py
+++ app1/models.py
@@ -21,3 +21,37 @@ class SPM_TASK(models.Model):
     status=models.CharField(max_length=20,choices=[("Pending","Pending"),("Completed","Completed")],default="Pending")
     created_at=models.DateTimeField(auto_now_add=True)
     
+class Senior_dev(models.Model):
+    sde_id=models.AutoField(primary_key=True)
+    sde_spm=models.ForeignKey(SPM,on_delete=models.CASCADE,related_name="sde_spm")
+    sde_cto=models.ForeignKey(CTO,on_delete=models.CASCADE,related_name="sde_cto")
+    name=models.CharField(max_length=100,blank=False,null=False)
+    password=models.CharField(max_length=100,blank=False,null=False)
+
+class UIUX(models.Model):
+    uiux_id=models.AutoField(primary_key=True)
+    uiux_spm=models.ForeignKey(SPM,on_delete=models.CASCADE,related_name="uiux_spm")
+    uiux_cto=models.ForeignKey(CTO,on_delete=models.CASCADE,related_name="uiux_cto")
+    name=models.CharField(max_length=100,blank=False,null=False)
+    password=models.CharField(max_length=100,blank=False,null=True)
+
+class DEVOPS(models.Model):
+    dops_id=models.AutoField(primary_key=True)
+    dops_spm=models.ForeignKey(SPM,on_delete=models.CASCADE,related_name="dops_spm")
+    dops_cto=models.ForeignKey(CTO,on_delete=models.CASCADE,related_name="dops_cto")
+    name=models.CharField(max_length=100,blank=False,null=False)
+    password=models.CharField(max_length=100,blank=False,null=False)
+
+class NE(models.Model):
+    ne_id=models.AutoField(primary_key=True)
+    ne_spm=models.ForeignKey(SPM,on_delete=models.CASCADE,related_name="ne_spm")
+    ne_cto=models.ForeignKey(CTO,on_delete=models.CASCADE,related_name="ne_cto")
+    name=models.CharField(max_length=100,blank=False,null=False)
+    password=models.CharField(max_length=100,blank=False,null=False)
+
+class Intern(models.Model):
+    intern_id=models.AutoField(primary_key=True)
+    intern_spm=models.ForeignKey(SPM,on_delete=models.CASCADE,related_name="intern_spm")
+    intern_cto=models.ForeignKey(CTO,on_delete=models.CASCADE,related_name="intern_cto")
+    name=models.CharField(max_length=100,blank=False,null=False)
+    password=models.CharField(max_length=100,blank=False,null=False)
